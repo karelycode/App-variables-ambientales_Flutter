4c88fb98b8cb0e91a1878b625641d3a6ab2352e1
karelycode <20041240@itdurango.edu.mx>
2024-04-27 20:00:53 -0600

    commit archivos

---
+++ lib/pages/archsvc.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:csv/csv.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server/gmail.dart';
+
+class CSVScreen2 extends StatelessWidget {
+
+  Future<void> exportDataToCSV() async {
+
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('variablesTH').get();
+
+    List<List<dynamic>> csvData = [];
+    csvData.add(['Fecha', 'Humedad', 'Temperatura']); // Cambia los nombres de los campos según tu colección
+    querySnapshot.docs.forEach((doc) {
+      csvData.add([doc['fecha'], doc['humedad'], doc['temperatura']]);
+    });
+
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    String csvFilePath = '$dir/humTemp.csv';
+    File csvFile = File(csvFilePath);
+
+    String csv = const ListToCsvConverter().convert(csvData);
+    await csvFile.writeAsString(csv);
+    print('Archivo CSV guardado en: $csvFilePath');
+
+
+    final smtpServer = gmail('tu correo del servicio de correos', 'contraseña'); //ver video para generar la clave
+
+    final message = Message()
+      ..from = Address('correo para recibir', 'Variables Ambientales')
+      ..recipients.add('correo para recibir') // Cambiar al correo del destinatario
+      ..subject = 'Archivo CSV'
+      ..text = 'Adjunto encontrarás el archivo CSV.'
+      ..attachments.add(FileAttachment(File(csvFilePath)));
+
+    // Enviar el correo electrónico
+    try {
+      final sendReport = await send(message, smtpServer);
+      print('Correo electrónico enviado: ${sendReport.toString()}');
+    } catch (e) {
+      print('Error al enviar el correo electrónico: $e');
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Enviar datos por correo electrónico'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: exportDataToCSV,
+          child: Text('Enviar por correo electrónico'),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/graphics.dart
+++ lib/pages/graphics.dart
@@ -1,49 +1,100 @@
-import 'dart:html';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
-class Graphic extends class name extends StatefulWidget {
-  Graphic ({Key? key}): super(key:key);
+class Graphic extends StatefulWidget {
 
-  @override
-  _MyGraphics createState(){
-    return _MyGraphics();
-  }
+Graphic({Key? key}) : super(key: key);
+
+@override
+_MyGraphics createState() {
+return _MyGraphics();
+}
 }
 
-class _MyGraphics extends State<Graphics> {
-  List<_variablesData> data =<_variablesData>[];
-  @override
-  void initState(){
-    getDataFromFireStore().then((results){
-      SchedulerBinding.instance!.addPostFrameCallBack((String){
-        setState((){});
-      });
-    });
-    super.initState();
-  }
-  Future<void> getDataFromFireStore() async{
-    var snapShotsValue =
-    await FirenaseFirestore.instance.collection('variablesTH').get();
-    List<_variablesData>((e.Data()['fecha']),(e.data()['temperatura'])));
-  }).toList();
-  setState
-}
-  @Override
-    Widget build(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of.context,
-        ),
-      );
-    }
-  }
+class _MyGraphics extends State<Graphic> {
+
+List< _variablesData> data = <_variablesData>[];
+
+@override
+void initState() {
+getDataFromFireStore().then((results) {
+SchedulerBinding.instance!.addPostFrameCallback((String) {
+setState(() {});
+});
+});
+super.initState();
 }
 
+Future<void> getDataFromFireStore() async {
+var snapShotsValue =
+await FirebaseFirestore.instance.collection('variablesTH').get();
+List<_variablesData> data1 = snapShotsValue.docs
+    .map((e) {
+return  _variablesData((e.data()['fecha']),(e.data()['temperatura']));
+}).toList();
+setState(() {
+data = data1;
+print("**"+data.toString());
+});
+}
 
-class _variablesData{
-  _variablesData(this.year, this.datos);
-  final String year;
-  final String datos;
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+backgroundColor: Theme.of(context).colorScheme.primary,
+title: const Text('Temperaturas'),
+),
+body: Column(children: [
+//Initialize the chart widget
+SfCartesianChart(
+primaryXAxis: CategoryAxis(),
+legend: Legend(isVisible: true),
+tooltipBehavior: TooltipBehavior(enable: true),
+series: <CartesianSeries<_variablesData, String>>[
+LineSeries<_variablesData, String>(
+dataSource: data,
+xValueMapper: (_variablesData datos, _) {
+return datos.year;
+},
+yValueMapper: (_variablesData datos, _) {
+return datos.datos;
+},
+name: 'Temperatura',
+// Enable data label
+dataLabelSettings: DataLabelSettings(isVisible: true))
+]),
+Expanded(
+child: data.isEmpty
+? Center(
+child: Text('No hay datos disponibles'),
+)
+    : SfSparkLineChart.custom(
+trackball: SparkChartTrackball(
+activationMode: SparkChartActivationMode.tap,
+),
+marker: SparkChartMarker(
+displayMode: SparkChartMarkerDisplayMode.all,
+),
+labelDisplayMode: SparkChartLabelDisplayMode.all,
+xValueMapper: (int index) {
+return data[index].year;
+},
+yValueMapper: (int index) {
+return data[index].datos;
+},
+dataCount: 5,
+),
+)
+]));
+}
+}
 
+class _variablesData {
+_variablesData(this.year, this.datos);
+final String year;
+final double datos;
 }
